a411ba77d4d9a40c385bd75e54d78484e49b2745
Pasupathy <ram-kumar.pasupathy@metlife.com>
2020-02-18 22:05:36 +0530

    Angalar crash course

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Route1Component } from './routes/route1/route1.component';
+import { Route2Component } from './routes/route2/route2.component';
+import { RoutesComponent } from './routes/routes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [  
+    {path: "route1", component: Route1Component},
+    {path: "route2", component: Route2Component},
+    {path: 'home',component: RoutesComponent, children:[
+        {path: "route1", component: Route1Component},
+        {path: "route2", component: Route1Component},       
+      ] 
+    }, 
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RoutesComponent } from './routes/routes.component';
+import { Route1Component } from './routes/route1/route1.component';
+import { Route2Component } from './routes/route2/route2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    RoutesComponent,
+    Route1Component,
+    Route2Component
   ],
   imports: [
     BrowserModule,
